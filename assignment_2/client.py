3c6df346d5f8ad65da6740bbe45c231825d9f9c8
Dichik <omeluan.dima@gmail.com>
2023-01-02 16:51:18 +0200

    assignment_3: update communication

--- assignment_2/client.py
+++ assignment_2/client.py
@@ -1,24 +1,39 @@
-import socket
+import socket, sys
 
 
 def client_program():
-    host = socket.gethostbyname("localhost")  # as both code is running on same pc
-    port = 1061  # socket server port number
+    if 1 <= len(sys.argv) <= 2:
+        host = sys.argv[1] if len(sys.argv) > 1 else socket.gethostbyname("localhost")
+        PORT = 1061
+        MAX = 65535
 
-    client_socket = socket.socket()  # instantiate
-    client_socket.connect((host, port))  # connect to the server
+        client_socket = socket.socket()
+        client_socket.connect((host, PORT))
+        print ('Client socket name is', client_socket.getsockname())
 
-    message = input(" -> ")  # take input
+        message = 'let\'s begin' 
+        delay = 0.1
+        while message.lower().strip() != 'bye':
+            client_socket.send(message.encode())
+            client_socket.settimeout(delay)
+            
+            try:
+                data = client_socket.recv(MAX).decode()
+            except socket.timeout:
+                delay *= 2 # wait even longer for the next request
+                if delay > 2.0:
+                    raise RuntimeError('I think the server is down')
+                print('Received from server: ' + data)
+            except:
+                raise
+            else:
+                print ('The server says', repr(data))
+            message = input("enter your answer -> ")
 
-    while message.lower().strip() != 'bye':
-        client_socket.send(message.encode())  # send message
-        data = client_socket.recv(1024).decode()  # receive response
-
-        print('Received from server: ' + data)  # show in terminal
-
-        message = input(" -> ")  # again take input
-
-    client_socket.close()  # close the connection
+        client_socket.close()
+    else:
+        print ('usage: python client.py [ <interface> ] ') 
+        sys.exit(2)
 
 
 if __name__ == '__main__':
--- assignment_2/server.py
+++ assignment_2/server.py
@@ -4,9 +4,14 @@ import random, socket, sys
 def server_program():
     MAX = 65535
     PORT = 1061
+    questions = dict()
+    
+    questions[0] = 'what is red?'
+    questions[1] = 'what is OOP?'
+    questions[2] = 'how to avoid annoying people?'
 
     if 1 <= len(sys.argv) <= 2:
-        host = socket.gethostbyname("localhost")
+        host = sys.argv[1] if len(sys.argv) > 1 else socket.gethostbyname("localhost")
         server = socket.socket()        
         server.bind((host, PORT))
         server.listen()
@@ -16,14 +21,15 @@ def server_program():
             data = conn.recv(MAX).decode()
             if data:
                 print ('The client at ', address, ' says:', repr(data))
-                message = 'Your data was %d bytes' % len(data)
+                # message = 'Your data was %d bytes' % len(data)
+                message = questions[random.randint(0, len(questions) - 1)]
                 conn.send(message.encode())
             else:
                 print ('Pretending to drop packet from', address)
                 break
         print('Stop listening...')
     else: 
-        print ('usage: client.py [ <interface> ] ') 
+        print ('usage: python server.py [ <interface> ] ') 
         sys.exit(2)
 
 
